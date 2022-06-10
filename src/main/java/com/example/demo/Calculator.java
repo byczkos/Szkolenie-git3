e27040fd0b9c2802207ca1c4635cdefb6c25c4a2
Dawid Byczkowski <35502248+byczkos@users.noreply.github.com>
2022-06-10 13:01:44 +0200

    Update Calculator.java

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return a + b;
     }
     
-    public int subtract(int a, int b) {
-        return a-b;
+    public int multiply(int a, int b) {
+        return a*b;
     }
 }
